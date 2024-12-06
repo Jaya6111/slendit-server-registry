c41ce4579749994597b32453c45b0f9d90f1dd78
Jaya6111 <136075867+Jaya6111@users.noreply.github.com>
2024-12-06 19:11:53 +0530

    Uploading file
    Creating server registry

---
+++ server-registry/server-registry/src/test/java/com/fq/slendit/ServerRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fq.slendit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
